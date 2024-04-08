8106ee48a851917f4497ffb2f097183f105bdc68
akash567-ak <125081789+akash567-ak@users.noreply.github.com>
2024-04-08 11:58:45 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ margin: 90, padding: 30, backgroundColor: "#ccc"}}>
+      <h1>Hello world , My app is on internet</h1>
+      </div>
+  );
+}
+
+export default App;
